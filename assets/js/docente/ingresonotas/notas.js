ac1ac64f3b15383c74b422e8fb77ff2b2d02e775
sebastianmga <61295332+sebastianmga@users.noreply.github.com>
2021-04-25 15:00:41 -0600

    Update notas

--- assets/js/docente/ingresonotas/notas.js
+++ assets/js/docente/ingresonotas/notas.js
@@ -1,52 +1,68 @@
 const db = firebase.firestore();
 
-// Formulario de Ingreso de notas
+// Formulario de Ingreso de notas.
 const frmGuardarNotas = document.getElementById('frmIngresoNotas');
 
-const updateStudent = (id, updateStudent) => db.collection('materia').doc(id).update(updateStudent)
-let idEstudiante = '';
+// Update notas de materia.
+const UpdateNotasMateria = 
+(id, UpdateNotasMateria) => db.collection('materia').doc(id).update(UpdateNotasMateria)
 
-frmGuardarNotas.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const estudiante = document.getElementById('').value;
-    const grado = document.getElementById('').value;
-    const materia = document.getElementById('gradoResponsable').value;
-    
-    const p1nota1 = frmGuardarNotas['inNota1'].value;
-    const p1nota2 = frmGuardarNotas['inNota2'].value;
-    const p1nota3 = frmGuardarNotas['inNota3'].value;
+// Id de documento con notas de materia.
+var idNotasMateria = '', materia = '', estudiante = '';
 
-    const p2nota1 = frmGuardarNotas['inNota1'].value;
-    const p2nota2 = frmGuardarNotas['inNota2'].value;
-    const p2nota3 = frmGuardarNotas['inNota3'].value;
+frmGuardarNotas.addEventListener('submit', async (e) => { e.preventDefault();
 
-    const p3nota1 = frmGuardarNotas['inNota1'].value;
-    const p3nota2 = frmGuardarNotas['inNota2'].value;
-    const p3nota3 = frmGuardarNotas['inNota3'].value;
+    // Obtener número de período.
+    const periodo = document.getElementById('selectForPeriodos').value;
+    console.log(periodo);
 
-    const profesor = document.getElementById('').value;
+    // 1. Obtener materia.
 
-    try {
-        
-        await updateStudent(idEstudiante, {
-            estudiante: estudiante,
-            grado: grado,
-            materia: materia,
+
+    // 2. Obtener nombre de estudiante.
+
+
+    // 3. Obtener id de notas de materia.
+
+
+    // Notas del período 1.
+    if (periodo == 1) {
+
+        const p1nota1 = frmGuardarNotas['inNota1'].value;
+        const p1nota2 = frmGuardarNotas['inNota2'].value;
+        const p1nota3 = frmGuardarNotas['inNota3'].value;
+
+        await UpdateNotasMateria(idNotasMateria, {
             p1nota1: p1nota1,
             p1nota2: p1nota2,
             p1nota3: p1nota3,
-            p1nota1: p2nota1,
-            p1nota2: p2nota2,
-            p1nota3: p2nota3,
-            p1nota1: p3nota1,
-            p1nota2: p3nota2,
-            p1nota3: p3nota3,
-            profesor: profesor
         });
-    
-        frmGuardarNotas.reset();
+    } 
 
-    } catch (error) {
-        console.log(error);
+    // Notas del período 2.
+    else if (periodo == 2) {
+        const p2nota1 = frmGuardarNotas['inNota1'].value;
+        const p2nota2 = frmGuardarNotas['inNota2'].value;
+        const p2nota3 = frmGuardarNotas['inNota3'].value;
+
+        await UpdateNotasMateria(idNotasMateria, {
+            p2nota1: p1nota1,
+            p2nota2: p1nota2,
+            p2nota3: p1nota3,
+        });
+    } 
+    
+    // Notas del período 3.
+    else {
+        const p3nota1 = frmGuardarNotas['inNota1'].value;
+        const p3nota2 = frmGuardarNotas['inNota2'].value;
+        const p3nota3 = frmGuardarNotas['inNota3'].value;
+
+        await UpdateNotasMateria(idNotasMateria, {
+            p3nota1: p1nota1,
+            p3nota2: p1nota2,
+            p3nota3: p1nota3,
+        });
     }
+    frmGuardarNotas.reset();
 });
