71a29bb04580cd7a5c34eef201fcf52370e0f5ae
Skliport <skliport@gmail.com>
2021-04-26 11:51:06 -0600

    i gotta poooo

--- assets/js/docente/perfil/perfil.js
+++ assets/js/docente/perfil/perfil.js
@@ -1,5 +1,6 @@
 const db = firebase.firestore();
 const frmPerfilDocente= document.getElementById('frmPerfilDocente');
+const card = document.getElementById('name');
 window.addEventListener("DOMContentLoaded", (e)=>{
     
     //mostrar perfil
@@ -19,7 +20,8 @@ function MostrarDatosUsuario() {
             frmPerfilDocente["birthDocente"].value=doc.data().fechNacimiento;
             frmPerfilDocente["emailDocente"].value=doc.data().emailDocente;
             frmPerfilDocente["expDocente"].value=doc.data().gradoEncargado;
-            
+            frmPerfilDocente["telDocente"].value=doc.data().telefono;
+            card["name"].value=doc.data().nombre;
             
             document.querySelectorAll("#frmPerfilDocente .form-group").forEach(el=>{
                 el.classList.add("is-filled");
