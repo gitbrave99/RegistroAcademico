d94543f64dae65c2876b032c107b80eb547cbc93
gitbrave99 <bezisaac@gmail.com>
2021-04-28 11:35:43 -0600

    agregado de report y boletanots docente

--- assets/js/docente/ingresonotas/boleta.js
+++ assets/js/docente/ingresonotas/boleta.js
@@ -18,11 +18,7 @@ window.addEventListener("DOMContentLoaded", async (e) => {
             querySnapshot.forEach((doc) => {
                 nmTechComP = doc.data().nombre;
                 titleGradeResponsable.value = doc.data().gradoEncargado;
-                ShowNotasStudentByMateriaAll(nmTechComP, "Sociales", "tbListEstNotasSociales", "btnAddNotSociales");
-                ShowNotasStudentByMateriaAll(nmTechComP, "Lenguaje", "tbListEstNotasLenguaje", "btnAddNotLenguaje");
-                ShowNotasStudentByMateriaAll(nmTechComP, "Matemáticas", "tbListEstNotasMatematica", "btnAddNotMatematicas");
-                ShowNotasStudentByMateriaAll(nmTechComP, "Ciencias", "tbListEstNotasCiencia", "btnAddNotCiencias");
-                ShowNotasStudentByMateriaAll(nmTechComP, "Inglés", "tbListEstNotasIngles", "btnAddNotIngles");
+                ShowNotasStudentByMateriaAll(nmTechComP, "Sociales", "tbListEstNotasSociales", "btnPrevImprsion");
             });
 
         })
@@ -70,58 +66,6 @@ async function GetNotasDeMateriaId(estudiante, materia) {
             return snapshot.docs.map(doc => doc.data().id);
         })
 }
-
-// Btn - Guardar notas.
-frmGuardarNotas.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const periodo = parseInt(document.getElementById('selectForPeriodos').value);
-    if (!isNaN(periodo)) {
-        idMateToguardar = frmGuardarNotas['btnGuardarNotas'].dataset.idmaterysel;
-        materia = frmGuardarNotas['btnGuardarNotas'].dataset.nmmateria;
-        estudiante = frmGuardarNotas['btnGuardarNotas'].dataset.nmstudent;
-        nmprofesor = frmGuardarNotas['btnGuardarNotas'].dataset.nmteacher;
-
-        if (periodo === 1) {
-
-            const p1nota1 = frmGuardarNotas['inNota1'].value;
-            const p1nota2 = frmGuardarNotas['inNota2'].value;
-            const p1nota3 = frmGuardarNotas['inNota3'].value;
-
-            console.log("target nmte", e.target.dataset.nmteacher);
-            await UpdateNotasMateria(idMateToguardar, {
-                p1nota1: p1nota1,
-                p1nota2: p1nota2,
-                p1nota3: p1nota3,
-            });
-        }
-        // Notas del período 2.
-        else if (periodo === 2) {
-            const p2nota1 = frmGuardarNotas['inNota1'].value;
-            const p2nota2 = frmGuardarNotas['inNota2'].value;
-            const p2nota3 = frmGuardarNotas['inNota3'].value;
-
-            await UpdateNotasMateria(idMateToguardar, {
-                p2nota1: p2nota1,
-                p2nota2: p2nota2,
-                p2nota3: p2nota3,
-            });
-        }
-        else if (periodo === 3) {
-            const p3nota1 = frmGuardarNotas['inNota1'].value;
-            const p3nota2 = frmGuardarNotas['inNota2'].value;
-            const p3nota3 = frmGuardarNotas['inNota3'].value;
-
-            await UpdateNotasMateria(idMateToguardar, {
-                p3nota1: p3nota1,
-                p3nota2: p3nota2,
-                p3nota3: p3nota3,
-            });
-        }
-        frmGuardarNotas.reset();
-        location.reload();
-    }
-});
-
 function truncNota(x, posiciones = 0) {
     var s = x.toString()
     var l = s.length
@@ -132,76 +76,96 @@ function truncNota(x, posiciones = 0) {
 
 
 
-function OnlyShowSubjects(doc, tbliMate) {
-    let tblistar = document.querySelectorAll(`#${tbliMate} tbody`)
+function OnlyShowSubjects(pNmesudin) {
     let totP1 = 0, totP2 = 0, totP3 = 0, totFinal = 0;
-    //PERIODO I
-    totP1 = ((doc.data().p1nota1 * 0.35) + (doc.data().p1nota2 * 0.35) + (doc.data().p1nota3 * 0.30));
-    totP2 = ((doc.data().p2nota1 * 0.35) + (doc.data().p2nota2 * 0.35) + (doc.data().p2nota3 * 0.30));
-    totP3 = ((doc.data().p3nota1 * 0.35) + (doc.data().p3nota2 * 0.35) + (doc.data().p3nota3 * 0.30));
-    totFinal = (totP1 + totP2 + totP3) / 3;
-    tblistar.innerHTML += `
-    <tr>
-        <td class="text-center">${doc.data().estudiante}</td>
-        <td class="text-center">${doc.data().p1nota1}</td>
-        <td class="text-center">${doc.data().p1nota2}</td>
-        <td class="text-center">${doc.data().p1nota3}</td>
-        <td class="text-primary text-center">${totP1}</td>
-        <td class="text-center">${doc.data().p2nota1}</td>
-        <td class="text-center">${doc.data().p2nota2}</td>
-        <td class="text-center">${doc.data().p2nota2}</td>
-        <td class="text-primary text-center">${totP2}</td>
-        <td class="text-center">${doc.data().p3nota1}</td>
-        <td class="text-center">${doc.data().p3nota2}</td>
-        <td class="text-center">${doc.data().p3nota3}</td>
-        <td class="text-primary text-center">${totP3}</td>
-        <td class="text-primary text-success">${totP3}</td>
-        <td>
-                <button type="button" class="btn btn-info green accent-4 btnAddNotSociales" data-nmTeacher="${doc.data().profesor}" data-materia="${doc.data().materia}" data-toggle="modal" data-target="#exampleModal">
-                    Agregar Nota
-                </button>
-                <a href="#"  type="button" class="btn btn-danger btn-sm btnDelStudent">clic</a>
-        </td>
-    </tr>`;
+    let tblistar = document.querySelector("#tbToPrintNtStudent");
+    let nmstutoprintn=document.getElementById("nmStudentToPrintNotas");
+    nmstutoprintn.innerHTML=pNmesudin;
+    db.collection("materia").where("estudiante", "==", pNmesudin)
+        .get()
+        .then((querySnapshot) => {
+            tblistar.innerHTML="";
+            querySnapshot.forEach((doc) => {
+                console.log(doc.data().materia);
+                totP1 = ((doc.data().p1nota1 * 0.35) + (doc.data().p1nota2 * 0.35) + (doc.data().p1nota3 * 0.30));
+                totP2 = ((doc.data().p2nota1 * 0.35) + (doc.data().p2nota2 * 0.35) + (doc.data().p2nota3 * 0.30));
+                totP3 = ((doc.data().p3nota1 * 0.35) + (doc.data().p3nota2 * 0.35) + (doc.data().p3nota3 * 0.30));
+                totFinal = (totP1 + totP2 + totP3) / 3;
+                tblistar.innerHTML += `
+                <tr>
+                    <td class="text-center">${doc.data().materia}</td>
+                    <td class="text-center">${truncNota(totFinal,2)}</td>
+                </tr>`;
+            });
+        });
 }
 
 // FUNCION FOR SHOW SOCIALES 
 function ShowNotasStudentByMateriaAll(pTeacher, pMateria, pTableMat, pBtnclassByMateria) {
     let tableMateriaSel = document.querySelector(`#${pTableMat} tbody`);
-    db.collection("materia").where("materia", "==", pMateria).where("profesor", "==", pTeacher)
+    db.collection("materia").where("profesor", "==", pTeacher).where("materia", "==", pMateria)
         .get()
         .then((querySnapshot) => {
             tableMateriaSel.innerHTML = "";
             querySnapshot.forEach((doc) => {
-                // console.log("en foreach", doc.data());
-                let totP1 = 0, totP2 = 0, totP3 = 0, totFinal = 0;
-                //PERIODO I
-                totP1 = ((doc.data().p1nota1 * 0.35) + (doc.data().p1nota2 * 0.35) + (doc.data().p1nota3 * 0.30));
-                totP2 = ((doc.data().p2nota1 * 0.35) + (doc.data().p2nota2 * 0.35) + (doc.data().p2nota3 * 0.30));
-                totP3 = ((doc.data().p3nota1 * 0.35) + (doc.data().p3nota2 * 0.35) + (doc.data().p3nota3 * 0.30));
-                totFinal = (totP1 + totP2 + totP3) / 3;
-                tableMateriaSel.innerHTML += `
-                <tr>
-                    <td class="text-center">${doc.data().estudiante}</td>
-                    <td class="text-center">${doc.data().p1nota1}</td>
-                    <td class="text-center">${doc.data().p1nota2}</td>
-                    <td class="text-center">${doc.data().p1nota3}</td>
-                    <td class="text-primary text-center">${truncNota(totP1, 2)}</td>
-                    <td class="text-center">${doc.data().p2nota1}</td>
-                    <td class="text-center">${doc.data().p2nota2}</td>
-                    <td class="text-center">${doc.data().p2nota3}</td>
-                    <td class="text-primary text-center">${truncNota(totP2, 2)}</td>
-                    <td class="text-center">${doc.data().p3nota1}</td>
-                    <td class="text-center">${doc.data().p3nota2}</td>
-                    <td class="text-center">${doc.data().p3nota3}</td>
-                    <td class="text-primary text-center">${truncNota(totP3, 2)}</td>
-                    <td class="text-primary text-success">${truncNota(totFinal, 2)}</td>
-                    <td>
-                            <button type="button" class="btn btn-info green accent-4 ${pBtnclassByMateria}" data-idmatselected="${doc.id}" data-nmstudent="${doc.data().estudiante}" data-nmteacher="${doc.data().profesor}" data-materia="${doc.data().materia}" data-toggle="modal" data-target="#mdAgregarNota">
-                                Agregar Nota
+                console.log(doc.data().estudiante);
+                db.collection("estudiante").where("nombre", "==", doc.data().estudiante)
+                    .get()
+                    .then((querySnapshotes) => {
+                        querySnapshotes.forEach((docet) => {
+                            tableMateriaSel.innerHTML += `
+                        <td class="text-center">${docet.data().nombre}</td>
+                        <td class="text-center">${docet.data().responsable}</td>
+                        <td class="text-center">${docet.data().telefono}</td>
+                        <td class="text-center">${docet.data().email}</td>
+                        <td class="text-center">${docet.data().direccion}</td>
+                        <td>
+                            <button type="button" class="btn btn-info green accent-4 ${pBtnclassByMateria}" data-nmstudent="${docet.data().nombre}" data-nmteacher="${doc.data().profesor}" data-toggle="modal" data-target="#mdPrevImpresion">
+                                Imprimir
                             </button>
-                    </td>
-                </tr>`;
+                        </td>
+                        `;
+                        })
+
+                        const allbtnpreimp = document.querySelectorAll(".btnPrevImprsion");
+                        allbtnpreimp.forEach((btn) => {
+                            btn.addEventListener("click", (e) => {
+                                console.log("clicked imprei");
+                                OnlyShowSubjects(e.target.dataset.nmstudent);
+                                
+                            });
+                        });
+                    });
+
+                // let totP1 = 0, totP2 = 0, totP3 = 0, totFinal = 0;
+                // //PERIODO I
+                // totP1 = ((doc.data().p1nota1 * 0.35) + (doc.data().p1nota2 * 0.35) + (doc.data().p1nota3 * 0.30));
+                // totP2 = ((doc.data().p2nota1 * 0.35) + (doc.data().p2nota2 * 0.35) + (doc.data().p2nota3 * 0.30));
+                // totP3 = ((doc.data().p3nota1 * 0.35) + (doc.data().p3nota2 * 0.35) + (doc.data().p3nota3 * 0.30));
+                // totFinal = (totP1 + totP2 + totP3) / 3;
+                // tableMateriaSel.innerHTML += `
+                // <tr>
+                //     <td class="text-center">${doc.data().estudiante}</td>
+                //     <td class="text-center">${doc.data().materia}</td>
+                //     <td class="text-center">${doc.data().p1nota2}</td>
+                //     <td class="text-center">${doc.data().p1nota2}</td>
+                //     <td class="text-center">${doc.data().p1nota3}</td>
+                //     <td class="text-primary text-center">${truncNota(totP1,2)}</td>
+                //     <td class="text-center">${doc.data().p2nota1}</td>
+                //     <td class="text-center">${doc.data().p2nota2}</td>
+                //     <td class="text-center">${doc.data().p2nota3}</td>
+                //     <td class="text-primary text-center">${truncNota(totP2,2)}</td>
+                //     <td class="text-center">${doc.data().p3nota1}</td>
+                //     <td class="text-center">${doc.data().p3nota2}</td>
+                //     <td class="text-center">${doc.data().p3nota3}</td>
+                //     <td class="text-primary text-center">${truncNota(totP3,2)}</td>
+                //     <td class="text-primary text-success">${truncNota(totFinal,2)}</td>
+                //     <td>
+                //             <button type="button" class="btn btn-info green accent-4 ${pBtnclassByMateria}" data-idmatselected="${doc.id}" data-nmstudent="${doc.data().estudiante}" data-nmteacher="${doc.data().profesor}" data-materia="${doc.data().materia}" data-toggle="modal" data-target="#mdAgregarNota">
+                //                 Imprimir
+                //             </button>
+                //     </td>
+                // </tr>`;
 
             });
 
