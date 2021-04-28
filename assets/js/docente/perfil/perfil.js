e75d2fc9603de2bffe9a742e58dc2f74b6861ed1
gitbrave99 <bezisaac@gmail.com>
2021-04-28 08:37:33 -0600

    agregado de email docente xq eta en undefined

--- assets/js/docente/perfil/perfil.js
+++ assets/js/docente/perfil/perfil.js
@@ -2,10 +2,8 @@ const db = firebase.firestore();
 const frmPerfilDocente= document.getElementById('frmPerfilDocente');
 const card = document.getElementById('name');
 window.addEventListener("DOMContentLoaded", (e)=>{
-    
     //mostrar perfil
     MostrarDatosUsuario();
-
 });
 
 
@@ -19,7 +17,7 @@ function MostrarDatosUsuario() {
             document.getElementById("nombreDocenteResumen").innerText = doc.data().nombre;
             frmPerfilDocente["userDocente"].value=doc.data().user;
             frmPerfilDocente["birthDocente"].value=doc.data().fechNacimiento;
-            frmPerfilDocente["emailDocente"].value=doc.data().emailDocente;
+            frmPerfilDocente["emailDocente"].value=doc.data().email;
             frmPerfilDocente["expDocente"].value=doc.data().gradoEncargado;
             frmPerfilDocente["telDocente"].value=doc.data().telefono;
             card["name"].value=doc.data().nombre;       
